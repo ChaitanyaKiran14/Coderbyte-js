955166c5b752c5a7f4c49815b4149655f9b85f31
Chaitanya <132725548+ChaitanyaKiran14@users.noreply.github.com>
2023-11-04 22:04:20 +0530

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -3,3 +3,18 @@ class Solution:
         return sorted(s) == sorted(t)
 
 
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return sorted(s) == sorted(t)
+
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+
+        for char in s:
+            s_dict[char] += 1
+
+        for char in t:
+            t_dict[char] += 1
+
+        return s_dict == t_dict
