bfcf77b9b270a18d2f8af1fc9ac13f80c8632c40
Chaitanya <132725548+ChaitanyaKiran14@users.noreply.github.com>
2023-11-20 12:59:26 +0530

    palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -5,3 +5,14 @@ let a = (strinngg) => {
 }
 const myString = "MADAM"
 console.log(a(myString))
+
+// reverse method is an array method. To reverse a string, you need to convert it to an array, reverse the array, and then join it back into a string.
+
+let a = (strinngg) => {
+    let val =   strinngg.toLowerCase().split('').reverse().join('');
+    return val === strinngg.toLowerCase()
+   
+}
+const myString = "madaM"
+console.log(a(myString))
+
