df944ca80def3f86b1fa7efd8d185686f0f5d05a
Chaitanya <132725548+ChaitanyaKiran14@users.noreply.github.com>
2023-11-26 23:00:45 +0530

    Nxtwave PrimeNumbers.py

--- Nxtwave PrimeNumbers.py
+++ Nxtwave PrimeNumbers.py
@@ -8,3 +8,14 @@ if factors == 0:
 else:
     print("Not Prime")
         
+
+#PrimeNumbers from M to N:
+m = int(input())
+n = int(input())
+for i in range(m, n+1):
+    factors = 0
+    for j in range(2, i-1):
+        if i%j == 0:
+            factors+=1
+    if factors == 0:
+        print(i)
