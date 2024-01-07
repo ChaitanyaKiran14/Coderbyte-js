97dfaf8b5792328093fbf9d1ae417dddd3976608
Chaitanya <132725548+ChaitanyaKiran14@users.noreply.github.com>
2024-01-07 19:24:51 +0530

    Update PrimeNumber.js

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -14,3 +14,28 @@ if (factors === 0) {
 } else {
     console.log("Not Prime");
 }
+*******************************************************
+//To get prime numbers in a range
+
+let isPrime = (start, end) => {
+    let primes = [];
+
+    for (let num = start; num <= end; num++) {
+        let factors = 0;
+
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                factors += 1;
+            }
+        }
+
+        if (factors === 0) {
+            primes.push(num);
+        }
+    }
+
+    return primes;
+}
+
+let primeNumbers = isPrime(11, 30);
+console.log("Prime Numbers:", primeNumbers);
