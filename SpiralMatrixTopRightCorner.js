4031d0ba8472792f117da6748cc7c4c1301f31b1
Chaitanya <132725548+ChaitanyaKiran14@users.noreply.github.com>
2024-01-10 08:53:57 +0530

    Create SpiralMatrixTopRightCorner.js

---
+++ SpiralMatrixTopRightCorner.js
@@ -0,0 +1,40 @@
+Starts From the top-right corner of the matrix
+let matrix = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16], [17, 18, 19, 20]];
+
+let res = [];
+let left = 0,
+  right = matrix[0].length;
+let top = 0,
+  bottom = matrix.length;
+
+while (left < right && top < bottom) {
+  // Traverse top row
+  for (let i = right - 1; i >= left; i--) {
+    res.push(matrix[top][i]);
+  }
+  top++;
+
+  // Traverse down column
+  for (let i = top; i < bottom; i++) {
+    res.push(matrix[i][left]);
+  }
+  left++;
+
+  if (!(left < right && top < bottom)) {
+    break;
+  }
+
+  // Traverse bottom row
+  for (let i = left; i < right; i++) {
+    res.push(matrix[bottom - 1][i]);
+  }
+  bottom--;
+
+  // Traverse right column
+  for (let i = bottom - 1; i >= top; i--) {
+    res.push(matrix[i][right - 1]);
+  }
+  right--;
+}
+
+console.log(res);
